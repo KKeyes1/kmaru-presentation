1cf9a1a47cfcd8f736dc13f4c20acd1545dcef2c
Ken Keyes <kkeyes1@cnm.edu>
2018-03-14 14:26:35 -0600

    quick spin up of presentation title slide

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>KMARU Presentation</title>
+
+
+		<link rel="stylesheet" href="style.css">
+		<!--		Bootstrap CSS-->
+		<link
+			rel="stylesheet"
+			href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+			crossorigin="anonymous"/>
+
+		<!--		Bootstrap JS Dependencies-->
+		<script
+			src="https://code.jquery.com/jquery-3.3.1.min.js"
+			integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			crossorigin="anonymous"></script>
+		<script
+			src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+			integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+			crossorigin="anonymous"></script>
+		<script
+			src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+			integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+			crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+		<div class="row h-100">
+			<div class="col">
+				<div class="jumbotron h-100 rounded-0" style="background-color: blueviolet;">
+					<div class="d-flex align-items-center flex-column justify-content-center h-100 text-white" id="joinedBackground">
+						<img class="card-img-top" src="kmaru-white.svg" alt="Card image cap" id="badgeLogo">
+					</div>
+				</div>
+			</div>
+			<div class="col my-auto" style="color: blueviolet;">
+				<div>
+					<h1>Project:</h1>
+					<h2>KMARU</h2>
+					<hr>
+					<h1>Team:</h1>
+					<h2>Tristan Bennett</h2>
+					<h2>Freddy Crawford</h2>
+					<h2>Kenneth Keyes</h2>
+					<h2>Anna Khamsamran</h2>
+					<hr>
+					<h1>URL: http://bit.ly/2FGbEtG</h1>
+					<h3>If you want to join in later visit the URL and sign up.</h3>
+					<h3>Check your e-mail for a validation message.</h3>
+					<h3>Sign in with your e-mail and password.</h3>
+					<h3>Wait for us to hand out our Board Id!</h3>
+				</div>
+			</div>
+		</div>
+
+
+	</body>
+</html>
